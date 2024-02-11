4726f7b229f85af50e7b16f7f5062ad2c3db3aff
TMEECH00 <esantaiwo77@gmail.com>
2024-02-12 00:48:01 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import css from './Header.module.css'
+import Logo from '../../assets/logo.png'
+import { CgShoppingBag } from 'react-icons/cg'
+import {GoThreeBars} from 'react-icons/go'
+
+const Header = () => {
+
+  const [ShowMenu, setShowMenu] = useState(true)
+
+  const toggleMenu = () => {
+    setShowMenu((ShowMenu)=>!ShowMenu)
+  }
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="" />
+        <span>meech</span>
+      </div>
+      
+      <div className={css.right}>
+
+        <div className={css.bars} onClick={toggleMenu}>
+          <GoThreeBars/>
+        </div>
+
+       
+          <ul className={css.menu} style={{display: ShowMenu? 'inherit': 'none'}}>
+             <li>Collection</li>
+             <li>Brands</li>
+             <li>New</li>
+             <li>Sales</li>
+             <li>ENGLISH</li>
+           </ul>
+        <input type="text" className={css.search} placeholder='Search'/>
+
+        <CgShoppingBag className={css.cart} />
+      </div>
+    </div>
+  )
+}
+
+export default Header
